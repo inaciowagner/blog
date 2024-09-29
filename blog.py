a25c3f97418100da335ef7817cd6710d77995817
Inácio Wagner <inacio@datacomrn.com.br>
2024-09-29 11:40:25 -0300

    estadao
    add trecho do estadao no iframe aninhado

--- blog.py
+++ blog.py
@@ -3,14 +3,14 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
-with open("blog.html", "r") as f:
+with open("blog.html", "r", encoding='utf-8') as f:
     html_string = f.read()
 
-#components.html(html_string, height=500)
+components.html(html_string, height=500)
 
-st.markdown(
-    """
-    <iframe src="blog.html" width="100%" height="100%"></iframe>
-    """,
-    unsafe_allow_html=True
-)
+#st.markdown(
+ #   """
+  #  <iframe src="blog.html" width="100%" height="100%"></iframe>
+   # """,
+    #unsafe_allow_html=True
+#)

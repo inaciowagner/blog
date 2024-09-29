67c0533d60957fc30a57c7ed62be065fd114a087
Inácio Wagner <inacio@datacomrn.com.br>
2024-09-29 18:20:22 -0300

    tudo certo
    cemitério de perus pronto

--- blog.py
+++ blog.py
@@ -3,20 +3,17 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
+# cabeçalho do blog
+st.html("<img src='https://raw.githubusercontent.com/inaciowagner/blog/refs/heads/main/casa_do_carvalho.png'>")
+st.html("<hr/>")
+
+
+# arquivo
 with open("blog.html", "r", encoding='utf-8') as f:
     html_string = f.read()
-
-st.html("<img src='casa_do_carvalho.png'>")    
-
-st.markdown('''
-    ###### Postado em: 29/09/2024 - 12:00
-
-''')
-components.html(html_string, height=3500)
-
-#st.markdown(
- #   """
-  #  <iframe src="blog.html" width="100%" height="100%"></iframe>
-   # """,
-    #unsafe_allow_html=True
-#)
+# data
+st.markdown(''' ###### Postado em: 29/09/2024 - 12:00 ''')
+#
+# 
+components.html(html_string, height=4000)
+st.html("<a href='https://l.instagram.com/?u=https%3A%2F%2Fbit.ly%2FFalarComInacio&e=AT3IhR-X_c2-jVZnNXoFLuKpJXR91OWiLXu4aQz2_vQJedXyPzwqtyFFgZa7QYidlhGAQ0PS4u2f1QQJX-IVTF7Qyl6YeO67vpiY1w' target='_blank'>Conheça meu Github</a>")

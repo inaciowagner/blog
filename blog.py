648b7feda0b09acbad38e546b8918e2497978e29
Inácio W. L. de Carvalho <inacio@datacomrn.com.br>
2025-11-30 14:23:43 -0300

    Modify blog layout and add webinar content
    Modificado texto para reforma tributária

--- blog.py
+++ blog.py
@@ -1,19 +1,43 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(layout="wide", page_icon="tree.png", page_title="Casa do Carvalho")
-
-# cabeçalho do blog
-st.html("<img src='https://raw.githubusercontent.com/inaciowagner/blog/refs/heads/main/casa_do_carvalho.png'>")
-st.html("<hr/>")
+st.set_page_config(layout="wide", page_icon="💰", page_title="Reforma Tributária")
 
 
-# arquivo
-with open("blog.html", "r", encoding='utf-8') as f:
-    html_string = f.read()
-# data
-st.markdown(''' ###### Postado em: 29/09/2024 - 12:00 ''')
-#
-# 
-components.html(html_string, height=4000)
-st.html("<a href='https://l.instagram.com/?u=https%3A%2F%2Fbit.ly%2FFalarComInacio&e=AT3IhR-X_c2-jVZnNXoFLuKpJXR91OWiLXu4aQz2_vQJedXyPzwqtyFFgZa7QYidlhGAQ0PS4u2f1QQJX-IVTF7Qyl6YeO67vpiY1w' target='_blank'>Conheça meu Github</a>")
+st.markdown(''' 
+ # WEBINAR REFORMA TRIBUTÁRIA
+## TOTVS CONSINCO
+
+### Anotações
+
+#### Agenda 
+1. Como preparar o PDV para a Reforma Tributária
+    ##### Estrutura e Motor Tributário
+    	a) Estrutura
+    	    * Adequando os processos da nova importação e exportação de vendas, alinhada à nova estrutura tributária.
+    
+          
+2. Detalhes completos no TOTVS Varejo Supermercado PDV
+
+
+            
+##### Obs:
+* atualizar os PDVs;
+* nossa versão atualmente 25.9.0.4;
+''')
+
+st.html("""
+        <h3>Conteudo Suplementar</h3> 
+        <ul>
+            <li>
+                <a href='https://tdn.totvs.com/pages/releaseview.action?pageId=1003982193'>Documentação Auxiliar</a>
+            </li>
+        </ul>
+        
+        """)
+
+st.image(
+    'imagens/Captura de tela de 2025-11-30 14-04-40.png',
+    caption='Comounidade no Whatsapp',
+    width=300
+)

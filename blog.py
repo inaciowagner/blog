340374c2238505b6477fe9d714482471ae9ba701
Inácio W. L. de Carvalho <inacio@datacomrn.com.br>
2025-11-30 22:39:49 -0300

    Atualizar o blog.py

--- blog.py
+++ blog.py
@@ -23,6 +23,11 @@ st.markdown('''
 * Integrações para linhas Consinco e RMS: ter acesso ao Configurador de Cenários Tributários e configurado cenários tributários;
 * Realizar o envio de cargas;
 * Ativar o parâmetro da Reforma Tributária
+---
+**Ativando o parâmetro:**
+1. Monitor -> Configurações -> Cargas -> Cargas para Monitor;
+2. Monitor -> Configurações -> Cargas -> Cargas para PDV;
+3. Monitor -> Configurações -> Configurações de PDVs -> DF-e -> Geral -> Ativa Reforma Tributária 
 
 ---
 ##### Obs:

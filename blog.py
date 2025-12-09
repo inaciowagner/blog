ad255876f9a259fcac14b84cf465f4c12edf2dfa
Inácio W. L. de Carvalho <inacio@datacomrn.com.br>
2025-12-09 08:41:08 -0300

    Update blog.py with community update on version 26.01
    Added community information regarding the discontinuation of version 25.01 and recommendations for upgrading to version 26.01.

--- blog.py
+++ blog.py
@@ -37,6 +37,32 @@ st.markdown('''
 * necessária 25.11.006;
 * nossa versão atualmente 25.9.0.4;
 * prazo 05/02/2026.
+---
+##### Informações da comunidade Whatsapp
+473 TOTVS Informa - Descontinuação da Versão 25.01 prorrogada
+
+Prezados(as) Clientes,
+
+A versão 26.01 dos produtos TOTVS Varejo Supermercados - Linhas Consinco, RMS e WMS será disponibilizada em janeiro de 2026 e trará importantes evoluções estruturais.
+
+Entre elas, destaca-se a adequação ao novo padrão de CNPJ Alfanumérico, cuja vigência inicia-se em julho de 2026.
+
+Diante disso, recomendamos o planejamento da atualização para esta versão ainda no primeiro semestre de 2026, garantindo assim plena conformidade com a nova legislação. Ressaltamos que essa adequação não está contemplada nas versões 25.01 e 25.07.
+
+Adicionalmente, entendendo o impacto da Reforma Tributária prevista para entrar em vigor em 01/01/2026, informamos que, de forma excepcional , prorrogaremos o encerramento do suporte para correções de bugs e atualizações legais da versão 25.01 até 28/02/2026 .
+
+Em resumo:
+
+* Até 28/02/2026 , é necessário que sua empresa esteja utilizando, no mínimo, a versão 25.07 .
+* Até 30/06/2026 , será obrigatório estar na versão 26.01 .
+
+Caso sua operação envolva customizações ou integrações , recomendamos antecipar o planejamento para garantir a compatibilidade com a versão 26.01.
+https://tdn.totvs.com/pages/releaseview.action?pageId=1014650613
+
+Permanecemos à disposição para esclarecer quaisquer dúvidas e para apoiá-lo(a) nesse processo de atualização.
+
+Atenciosamente,
+Equipe TOTVS Varejo Supermercados
 ''')
 
 st.html("""
